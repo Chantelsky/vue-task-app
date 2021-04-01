98954341cb382c7fd45041163a84f9229e33a675
Chantelsky <laver.chantelsky@gmail.com>
2021-04-01 18:23:45 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <current-time />
+  </div>
+</template>
+
+<script>
+import CurrentTime from "./components/CurrentTime.vue";
+
+export default {
+  name: "app",
+  components: {
+    CurrentTime,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
