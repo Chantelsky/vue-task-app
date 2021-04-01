a5626cec7814401a89df5a6bc9eda4e2edc54b4c
Chantelsky <laver.chantelsky@gmail.com>
2021-04-01 20:31:54 +1000

    adding tasks to the tasklist and displaying

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,67 @@
 <template>
   <div id="app">
     <current-time class="col-4" />
-    <task-input class="col-6" />
+    <task-input class="col-6" @add-task="addNewTask" />
+    <div class="col-12">
+      <div class="cardBox">
+        <div class="container">
+          <h2>My Tasks</h2>
+          <ul class="taskList">
+            <li
+              v-for="(taskItem, index) in taskList"
+              :key="`${index}_${Math.random()}`"
+            >
+              <input
+                type="checkbox"
+                :checked="!!taskItem.finishedAt"
+                @input="changeStatus(index)"
+              />
+              {{ taskItem.task }}
+              <span v-if="taskItem.finishedAt">
+                {{ taskItem.finishedAt }}
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import CurrentTime from "./components/CurrentTime.vue";
-import TaskInput from "./components/TaskInput";
-
+import TaskInput from "./components/TaskInput.vue";
 export default {
-  name: "app",
+  name: "TodoApp",
   components: {
     CurrentTime,
     TaskInput,
   },
+  data: () => ({
+    taskList: [],
+  }),
+  methods: {
+    addNewTask(task) {
+      this.$data.taskList.push({
+        task,
+        createdAt: Date.now(),
+        finishedAt: undefined,
+      });
+    },
+    changeStatus(taskIndex) {
+      const task = this.$data.taskList[taskIndex];
+      if (task.finishedAt) {
+        task.finishedAt = undefined;
+      } else {
+        task.finishedAt = Date.now();
+      }
+    },
+  },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.taskList li {
+  text-align: left;
 }
 </style>
