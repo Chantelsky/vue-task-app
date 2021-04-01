accb08c60bbcbee27270a0895d725ccca531c4ef
Chantelsky <laver.chantelsky@gmail.com>
2021-04-02 09:18:29 +1000

    formatted unix timestamp when task is done

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
           <h2>My Tasks</h2>
           <ul class="taskList">
             <li
-              v-for="(taskItem, index) in taskList"
+              v-for="(taskItem, index) in displayList"
               :key="`${index}_${Math.random()}`"
             >
               <input
@@ -18,7 +18,8 @@
               />
               {{ taskItem.task }}
               <span v-if="taskItem.finishedAt">
-                {{ taskItem.finishedAt }}
+                | Done at:
+                {{ formatDate(taskItem.finishedAt) }}
               </span>
             </li>
           </ul>
@@ -40,7 +41,29 @@ export default {
   data: () => ({
     taskList: [],
   }),
+  computed: {
+    displayList() {
+      return this.taskList;
+    },
+  },
   methods: {
+    formatDate(value) {
+      if (!value) return "";
+      if (typeof value !== "number") return value;
+
+      const browserLocale =
+        navigator.languages && navigator.languages.length
+          ? navigator.languages[0]
+          : navigator.language;
+      const intlDateTime = new Intl.DateTimeFormat(browserLocale, {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+      });
+      return intlDateTime.format(new Date(value));
+    },
     addNewTask(task) {
       this.$data.taskList.push({
         task,
